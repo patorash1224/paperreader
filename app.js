4bc10834f06f7b68b8b5b9dcef1b67b396b80b2a
patorash <syougi1224@gmail.com>
2024-12-08 10:39:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const input = document.querySelector('#input');
 const output = document.querySelector('#output');
 const btn = document.querySelector('#btn');
 
-btn.addEventListener('click',()=>{
+btn.addEventListener('click', () => {
     let text = input.value;
-    output.innerHTML=text.replace('\n','　');
-})
+    // 全ての改行を全角スペースに置換
+    output.innerHTML = text.replace(/\n/g, '　');
+});
